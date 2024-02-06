652e7f98cc5fcd6e209664626f693c98571424f0
Hearse <28695977+HearseDev@users.noreply.github.com>
2024-02-06 15:30:34 -0800

    Generate compile commands/debug build

--- build.sh
+++ build.sh
@@ -1,19 +1,44 @@
 #!/bin/bash
 
-# Exit on error
 set -e
 trap 'rm -rf build' EXIT
 
-# This script is used to build the project
 BIN_NAME="zsign"
+DEBUG=0
+BUILD_DIR="build"
+COMPILE_COMMANDS_FILE="compile_commands.json"
 
-# Check if cmake is installed
-if ! [ -x "$(command -v cmake)" ]; then
-  echo 'Error: cmake is not installed.' >&2
-#   exit 1
+function ensure_installed() {
+  if ! [ -x "$(command -v $1)" ]; then
+    echo "Error: $1 is not installed." >&2
+    exit 1
+  fi
+}
+
+for arg in "$@"
+do
+  if [ "$arg" == "-d" ]; then
+    DEBUG=1
+  fi
+done
+
+ensure_installed make
+ensure_installed cmake
+
+if [ $DEBUG -eq 1 ]; then
+  ensure_installed bear
+  echo "Debug build..."
+  rm -rf $BUILD_DIR && mkdir -p $BUILD_DIR && rm -f $BIN_NAME && rm -f $COMPILE_COMMANDS_FILE && pushd $BUILD_DIR
+  bear -- cmake -DCMAKE_BUILD_TYPE=Debug ..
+  bear -- make -j$(nproc)
+  mv $COMPILE_COMMANDS_FILE ..
+  mv $BIN_NAME ..
+  popd
+  exit 0
 fi
 
-rm -rf build && rm -f $BIN_NAME && mkdir -p build && pushd build
+echo "Building..."
+rm -rf $BUILD_DIR && mkdir -p $BUILD_DIR  && rm -f $BIN_NAME && pushd $BUILD_DIR
 cmake ..
 make -j$(nproc)
 mv $BIN_NAME ..
